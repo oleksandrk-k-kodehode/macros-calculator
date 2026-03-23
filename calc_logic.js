ee6e5d00496828ffc8ffef2a22a285c49428f6bf
oleksandr <oleksandr.k.kodehode@gmail.com>
2026-03-23 13:59:01 +0100

    added some js and a bit of other edits

--- calc_logic.js
+++ calc_logic.js
@@ -1,9 +1,10 @@
-function getData() {
-  const acitivity_level = document.querySelector(
-    'input[name="activity_level"]:checked',
-  )?.value;
-  const goals = document.querySelector('input[name="goal"]:checked')?.value;
-  console.log(acitivity_level);
-  console.log(goals);
+const form = document.getElementById("my_form");
+
+form.addEventListener("input", update_data);
+form.addEventListener("change", update_data);
+
+function update_data() {
+  const data = new FormData(form);
+  const obj = Object.fromEntries(data.entries());
+  return obj;
 }
-getData();
