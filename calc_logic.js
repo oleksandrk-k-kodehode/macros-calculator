59c47901420fbc82e93bd465808014f10c455e24
oleksandr <oleksandr.k.kodehode@gmail.com>
2026-03-23 21:54:43 +0100

    edited form and added js functions

--- calc_logic.js
+++ calc_logic.js
@@ -8,3 +8,29 @@ function update_data() {
   const obj = Object.fromEntries(data.entries());
   return obj;
 }
+
+function bmr(sex, age, height, weight) {
+  // W = weight (kg)
+  // H = height (cm)
+  // A = age (years)
+
+  // BMR=10W+6.25H−5A+5
+  if ((sex = man)) {
+    return 10 * weight + 6.25 * height - 5 * age + 5;
+  }
+  // BMR=10W+6.25H−5A−161
+  else {
+    return 10 * weight + 6.25 * height - 5 * age - 161;
+  }
+}
+
+function tdee(bmr, activity) {
+  // TDEE=BMR×Activity Level
+  /*
+    Sedentary (little/no exercise): × 1.2
+    Light activity (1–3 days/week): × 1.375
+    Moderate (3–5 days/week): × 1.55
+    Very active (6–7 days/week): × 1.725
+    Athlete/manual labor: × 1.9
+  */
+}
